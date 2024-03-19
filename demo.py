cb2528cbe1bfa5ab315c5cd650b9eb4da53435ad
Andrew Park <joohyung122002@gmail.com>
2024-03-19 11:20:59 -0700

    added dialogue based on storyboard

---
+++ demo.py
@@ -0,0 +1,86 @@
+from voice import Voice
+import time
+
+voice = Voice()
+
+voice.speak("Hello there! Thank for using BlindVision Fitness. Before we begin, I would like to know a little bit about yourself.")
+
+time.sleep(1)
+
+voice.speak("What is your name?")
+
+time.sleep(1)
+
+voice.speak("I heard Sean, is this correct?")
+
+time.sleep(1)
+
+voice.speak("What is your age?")
+
+time.sleep(1)
+
+voice.speak("I heard sixteen, is this correct?")
+
+time.sleep(1)
+
+voice.speak("Hello Sean, what exercise would you like to do?")
+
+time.sleep(1)
+
+voice.speak("The available exercises are: bench press, bicep curls, deadlifts, downward dog, planks, pushups, squats. Which would you like to do?")
+
+time.sleep(1)
+
+voice.speak("Begin")
+
+time.sleep(1)
+
+voice.speak("Lift your arm higher.")
+
+time.sleep(1)
+
+voice.speak("One")
+
+time.sleep(1)
+
+voice.speak("Lower your arm")
+
+time.sleep(1)
+
+voice.speak("Two")
+
+time.sleep(1)
+
+voice.speak("Three")
+
+time.sleep(1)
+
+voice.speak("Four")
+
+time.sleep(1)
+
+voice.speak("Five")
+
+time.sleep(1)
+
+voice.speak("Six")
+
+time.sleep(1)
+
+voice.speak("Seven")
+
+time.sleep(1)
+
+voice.speak("Eight")
+
+time.sleep(1)
+
+voice.speak("Nine")
+
+time.sleep(1)
+
+voice.speak("Ten")
+
+time.sleep(1)
+
+voice.speak("Exercise complete. Please say the name of the next exercise you would like to do when you are ready. Otherwise, you may power off if you are done.")
